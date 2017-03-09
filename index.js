8d841515b32995d8ef1f08875730d5f8afbaf8b1
Jason Harwig <jason@pinepointsoftware.com>
2017-03-09 10:55:17 -0600

    Add version selector if env vars available

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ module.exports = {
         config: function(config) {
             config.styles = config.styles || config.pluginsConfig['theme-default'].styles;
 
+            if ('VERSION_LIST' in process.env && 'VERSION_CURRENT' in process.env) {
+                config.pluginsConfig['theme-visallo'].versions = process.env['VERSION_LIST'].split(/[\s,]+/);
+                config.pluginsConfig['theme-visallo'].version = process.env['VERSION_CURRENT'];
+            }
+
             return config;
         }
     }
--- src/js/theme/index.js
+++ src/js/theme/index.js
@@ -28,6 +28,32 @@ function init() {
             sidebar.toggle();
         }
     });
+
+    var config = gitbook.state.config.pluginsConfig['theme-visallo'];
+    if ('versions' in config && 'version' in config) {
+        var versions = config.versions;
+        var version = config.version;
+        var usersPreferredVersion = gitbook.storage.get('version');
+        var currentVersion = (versions.indexOf(usersPreferredVersion) >= 0) ?
+            usersPreferredVersion : version;
+        var prefixRegex = /^\/(versions\/[^\/]+\/)?/;
+        toolbar.createButton({
+            text: 'Version: ' + currentVersion,
+            position: 'right',
+            className: 'version-selector',
+            dropdown: versions.map(function(version) {
+                return {
+                    text: version,
+                    className: version === currentVersion ? 'disabled' : '',
+                    onClick: function() {
+                        gitbook.storage.set('version', version);
+                        location.href = '/versions/' + version + '/' +
+                            location.pathname.replace(prefixRegex, '');
+                    }
+                };
+            })
+        });
+    }
 }
 
 gitbook.events.on('start', init);
